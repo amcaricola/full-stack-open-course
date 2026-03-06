4c319e8e62dacafd422e6930dc7ca10d26d5872b
Anthony M. Caricola <amcaricola@gmail.com>
2026-03-06 18:02:33 -0300

    Rename app and clean up starter files
    Update index.html title to "Restaurant feedback" and clean up starter project files: remove unused assets/styles (src/App.css, src/assets/react.svg, src/index.css) and adjust src/App.tsx and src/main.tsx accordingly. Also add generated package-lock.json. These changes rename the app and tidy the repository for the Restaurant feedback exercise.

--- PART 1/Exercises 1.6.-1.14/src/App.tsx
+++ PART 1/Exercises 1.6.-1.14/src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type statisticData = {
+  good: number;
+  neutral: number;
+  bad: number;
+};
+
+const Statistics = ({ data }: { data: statisticData }) => {
+  let { good, neutral, bad } = data;
+
+  let all = good + neutral + bad;
+  let average = (good - bad) / (good + neutral + bad);
+  let positive = (good / (good + neutral + bad)) * 100;
+
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad} </p>
+      <p>all {all} </p>
+      <p>average {average} </p>
+      <p>positive {positive} %</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handlerGood = () => {
+    let newGood = good + 1;
+    setGood(newGood);
+  };
+  const handlerNeutral = () => {
+    let newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+  };
+  const handlerBad = () => {
+    let newBad = bad + 1;
+    setBad(newBad);
+  };
+
+  const data: statisticData = { good, neutral, bad };
 
   return (
     <>
+      <h1>give feedback</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={handlerGood}>good</button>
+        <button onClick={handlerNeutral}>neutral</button>
+        <button onClick={handlerBad}>bad</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>statistics</h1>
+      <div>
+        <Statistics data={data} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
