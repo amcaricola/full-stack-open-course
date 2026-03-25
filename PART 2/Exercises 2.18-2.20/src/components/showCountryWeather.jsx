f81408ebd0e9def92d04ac9d12e3bb1a72cfd680
Anthony M. Caricola <amcaricola@gmail.com>
2026-03-24 22:45:57 -0300

    Externalize weather API key and update UI
    Move the OpenWeatherMap API key out of the code and into an untracked file (src/services/key.js) and add it to .gitignore. Add dotenv to package.json to support environment keys. Update services/countries to import the key and request weather with units=metric. Revamp showCountryWeather to display metric temperature, weather description and wind, remove manual conversion and noisy console logs. Minor cleanup/formatting in ShowCountries.jsx and remove obsolete db.json sample data.

--- PART 2/Exercises 2.18-2.20/src/components/showCountryWeather.jsx
+++ PART 2/Exercises 2.18-2.20/src/components/showCountryWeather.jsx
@@ -1,47 +1,42 @@
-import countriesService from "../services/countries";
-import { useState } from "react";
+import countriesService from '../services/countries';
+import { useState } from 'react';
 
 const ShowCountryWeather = ({ countries }) => {
-  const [weather, setWeather] = useState(null);
-  if (countries.length != 1) {
-    if (weather) setWeather(null);
-    return null;
-  }
+    const [weather, setWeather] = useState(null);
+    if (countries.length != 1) {
+        if (weather) setWeather(null);
+        return null;
+    }
 
-  const countrySelected = countries[0];
-  const [lon, lat] = countrySelected.capitalInfo.latlng;
+    const countrySelected = countries[0];
+    const [lon, lat] = countrySelected.capitalInfo.latlng;
 
-  function handleShowWeather() {
-    countriesService
-      .getCountryWeather(lon, lat)
-      .then((response) => {
-        console.log(response);
-        setWeather(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+    function handleShowWeather() {
+        countriesService
+            .getCountryWeather(lon, lat)
+            .then((response) => {
+                // console.log(response);
+                setWeather(response);
+            })
+            .catch((error) => {
+                // console.log(error);
+            });
+    }
 
-  var Celsius = 0;
-  if (weather) {
-    Celsius = (weather.main.temp - 32) * 0.55;
-    console.log(weather.main.temp);
-  }
-
-  return (
-    <>
-      <h2>Weather in {countrySelected.name.common}</h2>
-      {!weather ? (
-        <button onClick={handleShowWeather}>Show Weather</button>
-      ) : (
+    return (
         <>
-          <p>Temperature {Celsius.toFixed(2)} Celcius</p>
-          <p>Wind {weather.wind.speed} M/s</p>
+            <h2>Weather in {countrySelected.name.common}</h2>
+            {!weather ? (
+                <button onClick={handleShowWeather}>Show Weather</button>
+            ) : (
+                <>
+                    <p>Temperature: {weather.main.temp} Celcius</p>
+                    <p>Weather: {weather.weather[0].description}</p>
+                    <p>Wind: {weather.wind.speed} M/s</p>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default ShowCountryWeather;
--- PART 2/Exercises 2.18-2.20/src/services/countries.js
+++ PART 2/Exercises 2.18-2.20/src/services/countries.js
@@ -1,17 +1,20 @@
-import axios from "axios";
-const OWK = "b57a18db2771de4845bc26e141852706";
+import axios from 'axios';
+import WEATHER_API_KEY from './key';
+const OWK = WEATHER_API_KEY;
+
+console.log(OWK);
 
 const getAllCountries = async () => {
-  const response = await axios.get(
-    "https://studies.cs.helsinki.fi/restcountries/api/all",
-  );
-  return response.data;
+    const response = await axios.get(
+        'https://studies.cs.helsinki.fi/restcountries/api/all',
+    );
+    return response.data;
 };
 
 const getCountryWeather = async (lat, lon) => {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OWK}`;
-  const response = await axios.get(URL);
-  return response.data;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OWK}&units=metric`;
+    const response = await axios.get(URL);
+    return response.data;
 };
 
 export default { getAllCountries, getCountryWeather };
