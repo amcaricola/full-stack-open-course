96e58402176eaf5d74272861cdfb300d12f5a9e1
Anthony M. Caricola <amcaricola@gmail.com>
2026-04-20 22:27:52 -0400

    Implement blog analytics and add tests
    Implement favoriteBlog, mostBlogs and mostLikes in utils/list_helper.js (each returns false for non-array input). favoriteBlog returns the post with most likes; mostBlogs counts posts per author and returns the top author with blog count; mostLikes sums likes per author and returns the top author with total likes. Update tests in test/dummy.test.js: extract a shared list_of_blogs, remove duplicated fixtures, switch totalLikes test to use the shared list, and add tests for favoriteBlog, mostBlogs and mostLikes.

--- PART 4/Ejercicios 4.1/utils/list_helper.js
+++ PART 4/Ejercicios 4.1/utils/list_helper.js
@@ -18,11 +18,69 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
     if (Array.isArray(blogs)) {
-        var totalLikes = 0;
-        blogs.forEach((blog) => {
-            totalLikes += blog.likes ? blog.likes : 0;
+        var filteredBlog = blogs.sort((a, b) => {
+            return b.likes - a.likes;
         });
-        return totalLikes;
+        var blogToReturn = {
+            title: filteredBlog[0].title,
+            author: filteredBlog[0].author,
+            likes: filteredBlog[0].likes,
+        };
+        return blogToReturn;
+    }
+    return false;
+};
+
+const mostBlogs = (blogs) => {
+    if (Array.isArray(blogs)) {
+        var control = {};
+        blogs.forEach((blog) => {
+            if (control[blog.author]) {
+                control[blog.author] += 1;
+            } else {
+                control[blog.author] = 1;
+            }
+        });
+
+        let authorToReturn = {
+            author: '',
+            blogs: 0,
+        };
+
+        for (var author in control) {
+            if (control[author] > authorToReturn.blogs) {
+                authorToReturn.author = author;
+                authorToReturn.blogs = control[author];
+            }
+        }
+        return authorToReturn;
+    }
+    return false;
+};
+
+const mostLikes = (blogs) => {
+    if (Array.isArray(blogs)) {
+        var control = {};
+        blogs.forEach((blog) => {
+            if (control[blog.author]) {
+                control[blog.author] += blog.likes;
+            } else {
+                control[blog.author] = blog.likes;
+            }
+        });
+
+        let authorToReturn = {
+            author: '',
+            likes: 0,
+        };
+
+        for (var author in control) {
+            if (control[author] > authorToReturn.likes) {
+                authorToReturn.author = author;
+                authorToReturn.likes = control[author];
+            }
+        }
+        return authorToReturn;
     }
     return false;
 };
@@ -30,4 +88,7 @@ const favoriteBlog = (blogs) => {
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes,
 };
